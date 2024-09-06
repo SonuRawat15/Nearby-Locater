f8bc84f307f52e5aedfaa5eeed0d323972c96143
SonuRawat15 <147150872+SonuRawat15@users.noreply.github.com>
2024-09-06 09:43:28 +0530

    Create BookingRequest.java

---
+++ BookingRequest.java
@@ -0,0 +1,8 @@
+public class BookingRequest {
+    private Long userId;
+    private Long businessId;
+    private LocalDateTime bookingDate;
+    private String serviceDetails;
+
+    // Getters and Setters
+}
